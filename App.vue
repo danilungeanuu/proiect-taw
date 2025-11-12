c7a7a8cc7a5559a95ce0e6e9c3c336da0d0b2740
danilungeanu <danilungeanuu>
2025-11-12 17:04:44 +0200

    actualizare

--- App.vue
+++ App.vue
@@ -1,11 +1,13 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="text-[10px]">You did it!</h1>
+  <i class="bi bi-brightness-high"></i>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+@import "tailwindcss";
+@import "bootstrap-icons";
+
+</style>
